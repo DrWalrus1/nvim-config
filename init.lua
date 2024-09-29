b64be9f834c1c0cfd7482fcc3c17e125ec46c4d0
Luke Lynch <luke@lukelynch.io>
2024-09-29 12:03:42 +1000

    Update which-key config

--- init.lua
+++ init.lua
@@ -210,23 +210,20 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        { '<leader>c', name = '[C]ode' },
-        -- { '<leader>c_', hidden = true },
-        { '<leader>d', name = '[D]ocument' },
-        -- { '<leader>d_', hidden = true },
-        { '<leader>h', name = 'Git [H]unk' },
-        -- { '<leader>h_', hidden = true },
-        { '<leader>r', name = '[R]ename' },
-        -- { '<leader>r_', hidden = true },
-        { '<leader>s', name = '[S]earch' },
-        -- { '<leader>s_', hidden = true },
-        { '<leader>t', name = '[T]oggle' },
-        -- { '<leader>t_', hidden = true },
-        { '<leader>w', name = '[W]orkspace' },
+      require('which-key').add {
+
+        { '', group = '[W]orkspace' },
+        { '', group = 'Git [H]unk' },
+        { '', group = '[R]ename' },
+        { '', group = '[S]earch' },
+        { '', group = '[D]ocument' },
+        { '', group = '[C]ode' },
+        { '', group = '[T]oggle' },
+        { '', desc = '<leader>h', mode = 'v' },
+
         -- { '<leader>w_', hidden = true },
         -- Visual mode
-        { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
+        { '<leader>h', desc = 'Git [H]unk', mode = 'v', prefix = '' },
       }
     end,
   },
