d6f2b428b08ca301663e07226f275b1c92a45bb2
Luke Lynch <luke@lukelynch.io>
2024-12-08 01:19:17 +1100

    Modifying keymap

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,15 +3,15 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {
-    view_options = {
-      show_hidden = true,
-    },
-  },
+  opts = {},
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   config = function()
-    require('oil').setup {}
+    require('oil').setup {
+      view_options = {
+        show_hidden = true,
+      },
+    }
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
